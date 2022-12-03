6bae4aca16f61e44faed6c00cab95911726eb746
Eric V <ercvldez@gmail.com>
2022-12-03 15:51:26 -0800

    Updated edit sets

--- scripts/edit.js
+++ scripts/edit.js
@@ -8,46 +8,26 @@ const set = sets[setId];
 const cards = document.getElementById("cards");
 
 for (let id = 0; id < set.cards.length; id++) {
-  const card = set.cards[id];
-
-  const { term, definition } = card;
 
   //creates edit term and definition textboxes
+  const editTermLabel = document.createElement("label");
+  editTermLabel.textContent = "Term";
   const editTermTextBox = document.createElement("input");
   editTermTextBox.type = "text";
+  const editTermLineBreak = document.createElement("br");
 
+  const editDefinitionLabel = document.createElement("label");
+  editDefinitionLabel.textContent = "Defintion";
   const editDefinitionTextBox = document.createElement("input");
   editDefinitionTextBox.type = "text";
+
+  //creates line breaks
+  const lineBreak = document.createElement("br");
+  const newLineBreak = document.createElement("br");
   
   //creates edit button
   const editButton = document.createElement("button");
   editButton.textContent = "Edit";
-  
-  const cardFrame = document.createElement("button");
-  cardFrame.classList.add("card");
-
-  const container = document.createElement("div");
-  container.classList.add("card-inner");
-
-  const frontFrame = document.createElement("div");
-  frontFrame.classList.add("card-front");
-
-  const backFrame = document.createElement("div");
-  backFrame.classList.add("card-back");
-
-  const termLabel = document.createElement("h1");
-  termLabel.innerText = term;
-
-  const definitionLabel = document.createElement("p");
-  definitionLabel.innerText = definition;
-
-  cardFrame.onclick = function() {
-    if (container.classList.contains("card-flipped")) {
-      container.classList.remove("card-flipped");
-    } else {
-      container.classList.add("card-flipped");
-    }
-  }
 
   //edits term and definition
   editButton.onclick = function() {
@@ -64,16 +44,12 @@ for (let id = 0; id < set.cards.length; id++) {
     updateSet(sets);
   }
 
-  frontFrame.appendChild(termLabel);
-  backFrame.appendChild(definitionLabel);
-
-  container.appendChild(frontFrame);
-  container.appendChild(backFrame);
-
-  cardFrame.appendChild(container);
-
-  cards.appendChild(cardFrame);
-  cards.appendChild(editTermTextBox);
-  cards.appendChild(editDefinitionTextBox);
-  cards.appendChild(editButton);
+  document.body.appendChild(editTermLabel);
+  document.body.appendChild(editTermTextBox);
+  document.body.appendChild(editTermLineBreak);
+  document.body.appendChild(editDefinitionLabel);
+  document.body.appendChild(editDefinitionTextBox);
+  document.body.appendChild(editButton);
+  document.body.appendChild(lineBreak);
+  document.body.appendChild(newLineBreak);
 }
