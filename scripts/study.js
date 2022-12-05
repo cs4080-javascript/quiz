e3a1f2abd4e5aa5b8214bb25b32e9254515cc1e8
floor777 <bvfwra@gmail.com>
2022-12-04 22:39:16 -0800

    Added score at the end of completing quiz set

--- scripts/study.js
+++ scripts/study.js
@@ -8,12 +8,21 @@ const set = sets[setId];
 const root = document.getElementById("root");
 
 const cards = document.getElementById("cards");
+let correctCount = 0;
+let buttonsClicked = 0;
+let totalCount = set.cards.length;
+console.log(totalCount);
+
 
 for (const card of set.cards) {
   const { term, definition } = card;
   
   const cardFrame = document.createElement("button");
   cardFrame.classList.add("card");
+
+  const test = document.createElement("button");
+  test.classList.add("card");
+ 
 
   const container = document.createElement("div");
   container.classList.add("card-inner");
@@ -28,6 +37,41 @@ for (const card of set.cards) {
   termLabel.innerText = term;
 
   const definitionLabel = document.createElement("p");
+  const correctButton = document.createElement("button");
+  
+  correctButton.innerText = "Correct";
+  correctButton.className = "correct-buttons";
+  correctButton.onclick = function() {
+
+    correctCount++;
+    buttonsClicked++;
+    correctButton.disabled = true;
+    wrongButton.disabled = true;
+
+    if(buttonsClicked === totalCount) {
+      console.log(correctCount / totalCount);
+      window.alert(" Set complete. Your score was: "
+       + correctCount + "/" + totalCount + " or " + (correctCount/totalCount) * 100 + "%");
+    }
+    
+    
+  }
+  const wrongButton = document.createElement("button");
+  wrongButton.className = "wrong-buttons";
+  wrongButton.innerText = "Wrong";
+  wrongButton.onclick = function() {
+
+    buttonsClicked++;
+    wrongButton.disabled = true;
+    correctButton.disabled = true;
+    
+    if(buttonsClicked === totalCount) {
+      window.alert(" Set complete. Your score was: "
+       + correctCount + "/" + totalCount + " or " + (correctCount/totalCount) * 100 + "%");
+    }
+    
+  }
+
   definitionLabel.innerText = definition;
 
   cardFrame.onclick = function() {
@@ -40,11 +84,21 @@ for (const card of set.cards) {
 
   frontFrame.appendChild(termLabel);
   backFrame.appendChild(definitionLabel);
+ 
 
   container.appendChild(frontFrame);
   container.appendChild(backFrame);
+  
+
+ 
 
   cardFrame.appendChild(container);
+  cardFrame.appendChild(correctButton);
+  cardFrame.appendChild(wrongButton);
 
   cards.appendChild(cardFrame);
+  let correctPercentage = correctCount / totalCount;
+  
+
+ 
 }
